3e84ecd17b9c376b21edc908be052e50e4027b7c
Fly_Sunshine_J <857402355@qq.com>
2017-09-04 16:11:56 +0800

    新增代码

---
+++ MHDevelopExample/MHDevelopExample/Classes/MVC&MVVM/Base/BaseView/SUGoodsHeaderView.m
@@ -0,0 +1,22 @@
+//
+//  SUGoodsHeaderView.m
+//  MHDevelopExample
+//
+//  Created by senba on 2017/6/10.
+//  Copyright © 2017年 CoderMikeHe. All rights reserved.
+//
+
+#import "SUGoodsHeaderView.h"
+
+@implementation SUGoodsHeaderView
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+    
+    /// Setting the background color on UITableViewHeaderFooterView has been deprecated. Please use contentView.backgroundColor instead.
+    self.backgroundColor = SUGlobalGrayBackgroundColor;
+    self.contentView.backgroundColor = SUGlobalGrayBackgroundColor;
+}
+
+@end
